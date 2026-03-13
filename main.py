4b8c4dcb6b7a01a9b8818cb5a60c1daa0a96a91e
Joyanta Kumar <joyroy122004@gmail.com>
2026-03-13 13:12:16 +0600

    Navigated with vim keys, added cell grid in terminal, and reduced framerate

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import pygame
 from cell import Cell, Wall
-from constant import BG_COLOR, CURRENT_CELL_COLOR, COLS, ROWS, WINDOW
+from constant import BG_COLOR, CURRENT_CELL_COLOR, COLS, ROWS, WINDOW, FPS
 from random import choice
 
 pygame.init()
 pygame.display.set_caption("Maze Sim")
 clock = pygame.time.Clock()
-grid = [[Cell(row, col) for row in range(ROWS)] for col in range(COLS)]
+
+grid = []
+
+for row in range(ROWS):
+  current_row = []
+  for col in range(COLS):
+    current_cell = Cell(row, col)
+    current_row.append(current_cell)
+  grid.append(current_row)
+
+for row in grid:
+  for current_cell in row:
+    print([current_cell.row, current_cell.col], end="\t")
+  print()
+
+current_cell = grid[2][3]
 
 run = True
 while run:
@@ -16,13 +31,33 @@ while run:
     elif event.type == pygame.KEYDOWN:
       if event.key == pygame.K_ESCAPE:
         run = False
+      elif event.key == pygame.K_h and current_cell.col > 0:
+        current_cell = grid[current_cell.row][current_cell.col-1]
+      elif event.key == pygame.K_l and current_cell.col < COLS-1:
+        current_cell = grid[current_cell.row][current_cell.col+1]
+      elif event.key == pygame.K_j and current_cell.row < ROWS-1:
+        current_cell = grid[current_cell.row+1][current_cell.col]
+      elif event.key == pygame.K_k and current_cell.row > 0:
+        current_cell = grid[current_cell.row-1][current_cell.col]
+  
+  for row in grid:
+    for cell in row:
+      if cell == current_cell:
+        print('['+str(cell.row), str(cell.col)+']', end="\t")
+      else:
+        print('', cell.row, cell.col, '', end="\t")
+    print()
+  print()
 
   WINDOW.fill(BG_COLOR)
 
   for row in grid:
     for cell in row:
-      cell.draw(WINDOW)
+      if (cell == current_cell):
+        current_cell.draw(WINDOW, CURRENT_CELL_COLOR)
+      else:
+        cell.draw(WINDOW)
 
   # swaping the backstage with the front
   pygame.display.flip()
-  clock.tick(60)
+  clock.tick(FPS)
