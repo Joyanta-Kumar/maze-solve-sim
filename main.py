5875d0b515374d61dae297cf653909a6a1159133
Joyanta Kumar <joyroy122004@gmail.com>
2026-03-17 14:26:47 +0600

    Visualizing the BFS pattern. Next, another brnach will continue for DFS

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constant import WINDOW, CLOCK, FPS
 from constant import COLS, ROWS
-from constant import BG_COLOR, CELL_COLOR, VISITED_CELL_COLOR, CURRENT_CELL_COLOR, STACK_CELL_COLOR
+from constant import BG_COLOR, CELL_COLOR, VISITED_CELL_COLOR, CURRENT_CELL_COLOR, STACK_CELL_COLOR, NEXT_CELL_COLOR, NEIGHBOR_CELL_COLOR
 from view.maze import Maze
 from function import get_neighbors
 from random import choice
@@ -15,6 +15,7 @@ next_cell = maze.grid[0][0]
 queue = []
 current_cell = None
 stop_condition = False
+neighbors = []
 
 run = True
 while run:
@@ -27,7 +28,10 @@ while run:
       elif event.key == pygame.K_F5:
         maze.generate()
         queue = []
+        current_cell = None
+        stop_condition = False
         next_cell = maze.grid[0][0]
+        neighbors = []
   
   if not stop_condition:
     current_cell = next_cell
@@ -48,13 +52,20 @@ while run:
 
   for row in maze.grid:
     for cell in row:
-      cell.draw(WINDOW, VISITED_CELL_COLOR if cell.visited else CELL_COLOR)
+      if cell.visited:
+        cell.draw(WINDOW, VISITED_CELL_COLOR if cell.visited else CELL_COLOR)
   
   for cell in queue:
-    cell.draw(WINDOW, STACK_CELL_COLOR, padding=40, wall=False)
+    cell.draw(WINDOW, STACK_CELL_COLOR, padding=0, wall=False)
+
+  for cell in neighbors:
+    cell.draw(WINDOW, NEIGHBOR_CELL_COLOR, padding=0, wall=False)
 
   if current_cell:
-    current_cell.draw(WINDOW, CURRENT_CELL_COLOR, padding=10, wall=False)
+    current_cell.draw(WINDOW, CURRENT_CELL_COLOR, padding=0, wall=False)
+  
+  if next_cell:
+    next_cell.draw(WINDOW, NEXT_CELL_COLOR, padding=0, wall=False)
 
   pygame.display.flip()
   CLOCK.tick(FPS)
