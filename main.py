9515bc9d4412b8ff17cbbde47196d645077a0179
Joyanta Kumar <joyroy122004@gmail.com>
2026-03-17 13:38:37 +0600

    Visited the maze in main pygame loop

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 from constant import WINDOW, CLOCK, FPS
 from constant import COLS, ROWS
-from constant import BG_COLOR, CELL_COLOR
+from constant import BG_COLOR, CELL_COLOR, VISITED_CELL_COLOR, CURRENT_CELL_COLOR, STACK_CELL_COLOR
 from maze import Maze
+from function import get_neighbors
+from random import choice
 
 pygame.init()
 pygame.display.set_caption("Maze Sim")
@@ -10,8 +12,9 @@ pygame.display.set_caption("Maze Sim")
 maze = Maze(ROWS, COLS)
 
 next_cell = maze.grid[0][0]
-queue = []
-
+stack = []
+current_cell = None
+stop_condition = False
 
 run = True
 while run:
@@ -23,12 +26,34 @@ while run:
         run = False
       elif event.key == pygame.K_F5:
         maze.generate()
+        stack = []
+        next_cell = maze.grid[0][0]
+  
+  if not stop_condition:
+    current_cell = next_cell
+    if not current_cell.visited:
+      current_cell.visited = True
+      maze.visited_cell_count += 1
+      stop_condition = maze.visited_cell_count == maze.cell_count
+
+    neighbors = get_neighbors(current_cell, maze.grid, ignore_walls=False)
+    if len(neighbors) != 0:
+        next_cell = choice(neighbors)
+        stack.append(current_cell)
+    elif len(stack) != 0 and not stop_condition:
+      next_cell = stack.pop()
 
   WINDOW.fill(BG_COLOR)
 
   for row in maze.grid:
-    for cell in row: 
-      cell.draw(WINDOW, CELL_COLOR)
+    for cell in row:
+      cell.draw(WINDOW, VISITED_CELL_COLOR if cell.visited else CELL_COLOR)
+  
+  for cell in stack:
+    cell.draw(WINDOW, STACK_CELL_COLOR, padding=12, wall=False)
+
+  if current_cell:
+    current_cell.draw(WINDOW, CURRENT_CELL_COLOR, wall=False)
 
   pygame.display.flip()
   CLOCK.tick(FPS)
